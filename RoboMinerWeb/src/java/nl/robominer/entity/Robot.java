5c2366ea0edb67da13d092b96c359cee2bae5448
arnoud <arnoud@localhost>
2014-09-18 08:25:01 +0200

    Increase the robot speed to give more programming flexibility.

--- RoboMinerWeb/src/java/nl/robominer/entity/Robot.java
+++ RoboMinerWeb/src/java/nl/robominer/entity/Robot.java
@@ -42,6 +42,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
+ * Entity class for the Robot data.
  *
  * @author Arnoud Jagerman
  */
@@ -58,23 +59,35 @@ public class Robot implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * The robot id, primary key value.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
-    
+
+    /**
+     * The user owning the robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "usersId")
     private Users user;
-    
+
+    /**
+     * The name of the robot.
+     */
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 255)
     @Column(name = "robotName")
     private String robotName;
 
+    /**
+     * The source code of the program currently active on the robot.
+     */
     @Basic(optional = false)
     @NotNull
     @Lob
@@ -82,149 +95,281 @@ public class Robot implements Serializable {
     @Column(name = "sourceCode")
     private String sourceCode;
 
+    /**
+     * The reference to the source program currently linked to the robot.
+     * The linked source code will be copied to the active source code when
+     * the robot changes are applied, but only when the linked source code
+     * is valid and the robot is inactive.
+     */
     @Column(name = "programSourceId")
     private Integer programSourceId;
 
+    /**
+     * The ore container robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "oreContainerId")
     private RobotPart oreContainer;
 
+    /**
+     * The mining unit robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "miningUnitId")
     private RobotPart miningUnit;
 
+    /**
+     * The battery unit robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "batteryId")
     private RobotPart battery;
 
+    /**
+     * The memory module unit robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "memoryModuleId")
     private RobotPart memoryModule;
 
+    /**
+     * The cpu unit robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "cpuId")
     private RobotPart cpu;
 
+    /**
+     * The engine unit robot part active on this robot.
+     */
     @ManyToOne
     @NotNull
     @JoinColumn(name = "engineId")
     private RobotPart engine;
 
+    /**
+     * The recharge time for the robot, in seconds.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "rechargeTime")
     private int rechargeTime;
-    
+
+    /**
+     * The ore capacity.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "maxOre")
     private int maxOre;
-    
+
+    /**
+     * The maximum amount of ore the robot is able to mine each cycle. Please
+     * note that the amount of ore mined in one cycle also cannot exceed
+     * halve the ore available on a square.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "miningSpeed")
     private int miningSpeed;
-    
+
+    /**
+     * The maximum number of turns the robot can operate before the battery
+     * is depleted.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "maxTurns")
     private int maxTurns;
-    
+
+    /**
+     * The maximum size of a program that can be applied to this robot.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "memorySize")
     private int memorySize;
-    
+
+    /**
+     * The maximum number of program instructions the robot can process
+     * each cycle. When the program doesn't activate a robot action like
+     * move or mine before that, the robot will sit idle for that cycle and
+     * the program processing will continue next cycle.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "cpuSpeed")
     private int cpuSpeed;
-    
+
+    /**
+     * The maximum distance the robot can move forward each cycle.
+     * When a move() instruction with a larger number is executed, the robot
+     * moves this distance forward in one cycle and the rest of the
+     * distance in the next cycle(s).
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "forwardSpeed")
     private double forwardSpeed;
-    
+
+    /**
+     * The maximum distance the robot can move backward each cycle.
+     * The same mechanism applies as for forward movement.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "backwardSpeed")
     private double backwardSpeed;
-    
+
+    /**
+     * The maximum rotation a robot can execute each cycle. When a rotate()
+     * with a larger angle is executed, the robot is rotated this angle in
+     * one cycle and the rest of the requested angle in the next cycle(s).
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "rotateSpeed")
     private int rotateSpeed;
-    
+
+    /**
+     * The size of the robot.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "robotSize")
     private int robotSize;
-    
+
+    /**
+     * When in the future, the robot is currently recharging till this time
+     * is passed.
+     */
     @Column(name = "rechargeEndTime")
     @Temporal(TemporalType.TIMESTAMP)
     private Date rechargeEndTime;
 
+    /**
+     * When in the future, the robot is currently mining till this time is
+     * passed.
+     */
     @Column(name = "miningEndTime")
     @Temporal(TemporalType.TIMESTAMP)
     private Date miningEndTime;
 
+    /**
+     * The total number of mining runs this robot executed so far.
+     */
     @Basic(optional = false)
     @NotNull
     @Column(name = "totalMiningRuns")
     private int totalMiningRuns;
 
+    /**
+     * The total amounts of ore this robot mined so far.
+     */
     @OneToMany
     @JoinColumn(name = "RobotLifetimeResult.robotId")
     private List<RobotLifetimeResult> robotLifetimeResultList;
 
+    /**
+     * Default constructor.
+     */
     public Robot() {
     }
 
+    /**
+     * Retrieve the primary key value of the robot.
+     * 
+     * @return The id value of the robot.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Retrieve the owner of the robot.
+     * 
+     * @return The owner of the robot.
+     */
     public Users getUser() {
         return user;
     }
 
+    /**
+     * Change the owner of the robot.
+     * 
+     * @param user The new owner of the robot.
+     */
     public void setUser(Users user) {
         this.user = user;
     }
 
+    /**
+     * Retrieve the name of the robot.
+     * 
+     * @return The name of the robot.
+     */
     public String getRobotName() {
         return robotName;
     }
 
+    /**
+     * Change the name of the robot.
+     *
+     * @param robotName The new name of the robot.
+     */
     public void setRobotName(String robotName) {
         this.robotName = robotName;
     }
 
-    public String getSourceCode() {
-        return sourceCode;
-    }
-    
+    /**
+     * Update the currently active source code for this robot. This program
+     * source should always be valid.
+     *
+     * @param sourceCode The new program source.
+     */
     public void setSourceCode(String sourceCode) {
         this.sourceCode = sourceCode;
     }
-    
+
+    /**
+     * Retrieve the program source id this robot is linked to. Unlike the
+     * sourceCode field, the linked code could currently be invalid or too
+     * large to fit in the robot memory.
+     *
+     * @return The program source id this robot is linked to.
+     */
     public Integer getProgramSourceId() {
         return programSourceId;
     }
 
+    /**
+     * Update the program source id this robot is linked to.
+     * 
+     * @param programSourceId The new program source id this robot is linked to.
+     */
     public void setProgramSourceId(Integer programSourceId) {
         this.programSourceId = programSourceId;
     }
 
+    /**
+     * Retrieve the ore container robot part linked to this robot.
+     *
+     * @return The ore container robot part linked to this robot.
+     */
     public RobotPart getOreContainer() {
         return oreContainer;
     }
-    
+
+    /**
+     * Update the ore container robot part linked to this robot.
+     *
+     * @param oreContainer The ore container robot part linked to this robot.
+     */
     public void setOreContainer(RobotPart oreContainer) {
         this.oreContainer = oreContainer;
     }
@@ -471,10 +616,10 @@ public class Robot implements Serializable {
                     cpu.getCpuCapacity() + engine.getCpuCapacity());
         
         setMaxTurns(batteryCapacity / powerUsage);
-        
-        setForwardSpeed(forwardCapacity / weight);
-        setBackwardSpeed(backwardCapacity / weight);
-        setRotateSpeed((int)(10 * rotateCapacity / weight));
+
+        setForwardSpeed(3 * forwardCapacity / weight);
+        setBackwardSpeed(3 * backwardCapacity / weight);
+        setRotateSpeed((int)(20 * rotateCapacity / weight));
 
         setRobotSize((int) Math.pow(volume, 0.33));
     }
